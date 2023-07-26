414dc02335946a0e23718a3e0edf65b647766f8b
einsteinnnamah <einsteinnnamah@gmail.com>
2023-07-26 03:05:57 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import apple from "../src/assets/apple.png"
+import google from "../src/assets/google.png"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='background'>
+      <div className='box1'>
+      <div className='box2 inner'>ErrandBrij</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className='box2'>We are not just seeing delivery, we see you!</div>
+    <div>
+      <img width={120} src={google} />
+      <img width={120} src={apple} />
+    </div>
+    </div>
   )
 }
 
